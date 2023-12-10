f02a024c69737f4bfddc7fee662aa923e16f3175
chaoranzuo <15269876632@163.com>
2023-12-10 10:16:36 +0800

    feat: error测试

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,15 @@
-// app/dashboard/page.js
-async function getData() {
-    await new Promise((resolve) => setTimeout(resolve, 3000))
-    return {
-      message: 'Hello, Dashboard!',
-    }
-  }
-  export default async function DashboardPage(props) {
-    const { message } = await getData()
-    return <h1>{message}</h1>
-  }
+"use client";
+// dashboard/page.js
+import React from "react";
+
+export default function Page() {
+  const [error, setError] = React.useState(false);
+
+  const handleGetError = () => {
+    setError(true);
+  };
+
+  return (
+    <>{error ? Error() : <button onClick={handleGetError}>Get Error</button>}</>
+  );
+}
