4f65916ab5274fc81280f6809923f56d51e2f182
chaoranzuo <15269876632@163.com>
2023-12-10 16:38:53 +0800

    delete: 删除无用案例

--- app/api/route.js
+++
@@ -1,9 +0,0 @@
-// app/api/route.js
-import { NextResponse } from 'next/server'
- 
-export async function GET() {
-  const res = await fetch('https://dog-api.kinduff.com/api/facts')
-  const data = await res.json()
- 
-  return NextResponse.json({ data })
-}
