bcb7ab332d8f48d05c87445d50b5a86a510ad2fd
chaoranzuo <15269876632@163.com>
2023-12-10 10:13:29 +0800

    feat: loading测试

---
+++ app/dashboard/about/loading.js
@@ -0,0 +1,4 @@
+// app/dashboard/loading.js
+export default function AboutLoading() {
+    return <>Loading about...</>
+  }
--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,3 +1,11 @@
-export default function Pages() {
-    return <h1>Hello, DashBoard!</h1>
+// app/dashboard/page.js
+async function getData() {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    return {
+      message: 'Hello, Dashboard!',
+    }
+  }
+  export default async function DashboardPage(props) {
+    const { message } = await getData()
+    return <h1>{message}</h1>
   }
